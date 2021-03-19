7a0bb0ed0072f7fac827ede25891a31adb5067fd
Sarah Smith <s.smith.nz@gmail.com>
2021-03-20 12:35:19 +1300

    initial commit

---
+++ filterByTerm.js
@@ -0,0 +1,59 @@
+describe("Filter function", () => {
+  test("it should filter by a search term (link)", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+
+    const output = [{ id: 3, url: "https://www.link3.dev" }];
+
+    expect(filterByTerm(input, "link")).toEqual(output);
+
+    expect(filterByTerm(input, "LINK")).toEqual(output);
+  });
+});
+
+
+describe('search uRl', () => {
+  test ("it should test for the search term (uRl)", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+    const output = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" }
+    ];
+
+    expect(filterByTerm(input, "url")).toEqual(output);
+
+  })
+})
+describe('error thrown when empty searchTerm', () => {
+  test ("test for an empty searchTerm", () => {
+    const input = [];
+    expect(() => {filterByTerm(input, "");
+     }).toThrowError(Error('search cannot be empty'));
+
+  });
+})
+
+
+function filterByTerm(inputArr, searchTerm) {
+  if (!searchTerm) throw Error('search cannot be empty');
+  const regex = new RegExp(searchTerm, "i");
+  return inputArr.filter(function(arrayElement) {
+    return arrayElement.url.match(regex);
+  });
+}
+
+// function filterByTerm(inputArr, searchTerm) {
+//   if (!searchTerm) throw Error("searchTerm cannot be empty");
+//   if (!inputArr.length) throw Error("inputArr cannot be empty"); // new line
+//   const regex = new RegExp(searchTerm, "i");
+//   return inputArr.filter(function(arrayElement) {
+//     return arrayElement.url.match(regex);
+//   });
+// }
